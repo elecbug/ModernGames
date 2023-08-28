0f74387a5810934956f07f028c27002a0c599603
deveb1479@gmail.com <deveb1479@gmail.com>
2023-08-28 18:03:23 +0900

    Add project files.

---
+++ ModernGames/MyControls/ExitButton.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ModernGames.MyControls
+{
+    public class ExitButton : PictureBox
+    {
+        public ExitButton() : base() 
+        {
+            this.Click += ExitButtonClick;
+            this.Image = Properties.Image.Exit;
+            this.SizeMode = PictureBoxSizeMode.StretchImage;
+        }
+
+        private void ExitButtonClick(object? sender, EventArgs e)
+        {
+            Control control = (sender as Control)!;
+            
+            while (control.GetType().BaseType != typeof(Form))
+            {
+                control = control.Parent;
+            }
+
+            (control as Form)!.Close();
+        }
+    }
+}
---
+++ ModernGames/MyForms/MainForm.cs
@@ -0,0 +1,18 @@
+using ModernGames.MyControls;
+
+namespace ModernGames
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+
+            ExitButton exitButton = new ExitButton()
+            { 
+                Parent = this,
+                Size = new Size(50, 50),
+            };
+        }
+    }
+}
