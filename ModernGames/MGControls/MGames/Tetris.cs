8d89b4924db364022dcb51c40b70e784cbebce8c
deveb1479@gmail.com <deveb1479@gmail.com>
2023-08-29 12:58:51 +0900

    tetris temp - 1

--- ModernGames/MGControls/MGames/Tetris.cs
+++ ModernGames/MGControls/MGames/Tetris.cs
@@ -1,13 +1,101 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace ModernGames.MGControls.MGames
 {
-    public class Tetris : BaseGamePanel
+    public class Tetris : TimerBaseGamePanel
     {
+        /// <summary>
+        /// 게임판 가로 길이
+        /// </summary>
+        private int X_LENGTH = 10;
+        /// <summary>
+        /// 게임판 세로 길이 (실제로는 20, 데스 존 위의 Floating 공간을 위함)
+        /// </summary>
+        private int Y_LENGTH = 25;
+        /// <summary>
+        /// 데스 라인, 이 위에 블럭이 Building되면 게임 종료
+        /// </summary>
+        private int DEATH_LINE = 20;
+        /// <summary>
+        /// 블럭의 종류
+        /// </summary>
+        private int BlockCount = 7;
+
+        /// <summary>
+        /// 게임이 진행되는 2차원 공간
+        /// </summary>
+        private Block[,] Space;
+        /// <summary>
+        /// 다음 블록들의 리스트, 7개를 각각 하나씩 채우며 진행, 모두 비었을 때 다시 채움
+        /// </summary>
+        private List<Block> NextList;
+
+        /// <summary>
+        /// 테트리스 게임 시작 생성자
+        /// </summary>
+        /// <param name="level"> 게임의 난이도(0~99) </param>
+        public Tetris(int level) : base("Tetris", "The Modern Tetris", 100 - level)
+        {
+            this.Space = new Block[X_LENGTH, Y_LENGTH];
+            this.NextList = new List<Block>();
+        }
+
+        /// <summary>
+        /// NextList의 원소가 없는 경우 하나씩 채움
+        /// </summary>
+        private void NextListSetting()
+        {
+            if (this.NextList.Count != 0)
+            {
+                Debug.WriteLine("다음 블록 리스트가 비지 않았는데 리스트를 채우려 했습니다.");
+                return;
+            }
+
+            List<Block> list = new List<Block>(new Block[BlockCount]);
+            
+            for (int i = 0; i < BlockCount; i++)
+            {
+                list[i] = Block.Building | (Block)(4 * Math.Pow(2, i));
+            }
+
+            int n = list.Count;
+
+            while (n > 1)
+            {
+                n--;
+                int k = new Random(DateTime.Now.Millisecond).Next(n + 1);
+                Block value = list[k];
+                list[k] = list[n];
+                list[n] = value;
+            }
+
+            this.NextList = list;
+        }
+
+        /// <summary>
+        /// 블록의 종류, 공간은 0 ~ 2 | 4 ~ 256으로 구성됨
+        /// </summary>
+        private enum Block 
+        {
+            Empty = 0,
+            Floating = 1,
+            Building = 2,
+
+            LBlock = 4,
+            ReverseL = 8,
+            ZBlock = 16,
+            ReverseZ = 32,
+            TBlock = 64,
+            BoxBlock = 128,
+            IBlock = 256,
+        }
+
+
         protected override void End()
         {
             throw new NotImplementedException();
---
+++ ModernGames/MGControls/MGames/TimerBaseGamePanel.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Timer = System.Windows.Forms.Timer;
+
+namespace ModernGames.MGControls.MGames
+{
+    /// <summary>
+    /// 모든 실시간 게임의 베이스가 되는 추상 게임 패널
+    /// </summary>
+    public abstract class TimerBaseGamePanel : Panel
+    {
+        public new string Name { get; protected set; }
+        public string Description { get; protected set; }
+
+        public Timer Timer { get; private set; }
+
+        /// <summary>
+        /// 실시간 게임 생성자
+        /// </summary>
+        /// <param name="name"> 표시되는 게임 이름 </param>
+        /// <param name="description"> 게임 설명 문구 </param>
+        /// <param name="interval"> 프레임이자 게임의 진행 스피드 </param>
+        public TimerBaseGamePanel(string name, string description, int interval)
+        {
+            this.Name = name;
+            this.Description = description;
+            this.Timer = new Timer() 
+            {
+                Interval = interval,
+            };
+        }
+
+        /// <summary>
+        /// 게임을 시작, Start 후 Replace를 Tick 시간마다 호출
+        /// </summary>
+        public void Open()
+        {
+            Start();
+
+            this.Timer.Tick += (s, e) =>
+            {
+                Replace();
+            };
+            this.Timer.Start();
+        }
+
+        /// <summary>
+        /// Replace를 멈추고 End를 호출
+        /// </summary>
+        public void Close()
+        {
+            this.Timer.Stop();
+            End();
+        }
+
+        /// <summary>
+        /// 게임을 멈추거나 재시작
+        /// </summary>
+        /// <param name="wait"> true라면 정지, false라면 재시작 </param>
+        public void Wait(bool wait)
+        {
+            if (wait)
+                this.Timer.Stop();
+            else
+                this.Timer.Start();
+        }
+
+        /// <summary>
+        /// 게임이 최초 시작될 때 한번 호출되는 메서드
+        /// </summary>
+        protected abstract void Start();
+        /// <summary>
+        /// Timer의 Interval이 지날 때마다 호출되는 메서드
+        /// </summary>
+        protected abstract void Replace();
+        /// <summary>
+        /// 게임을 끝낼 때 한번 호출되는 메서드
+        /// </summary>
+        protected abstract void End();
+    }
+}
